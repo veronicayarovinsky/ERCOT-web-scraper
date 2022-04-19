4f9cb8092694921ef32981e0bef71eb1f305b455
Veronica Yarovinsky <veronicayarovinsky@Veronicas-MacBook-Air.local>
2022-04-19 13:32:46 -0600

    minor fixes and README updated

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ERCOT-web-scraper
 
+The ERCOT-web-scraper is a program that downloads LMPs by Resource Nodes, Load Zones and Trading Hubs from [the ERCOT website](https://www.ercot.com/mktinfo/prices).
+* The data is stored as csv files named exactly as on the ERCOT website (without the .zip)
+* If files downloaded by this program exist in the given pathname, the program only downloads the new data files (no repeat files).
+
+
+### How to run it:
+Edit the desired pathname (`PATH_NAME`) in the code, then from terminal run either the command `python3 scraper.py` or `python scraper.py` depending on the installed python version.
+
+
+### Limitations:
+* No file organization
+* Path for the folder to save data must be edited in the code
+* No way to deal with server crashes or nonexistent pathnames
--- scraper.py
+++ scraper.py
@@ -8,15 +8,14 @@ import zipfile, io
 import re
 import glob
 
-
 LMP_DATA_LINK = "http://mis.ercot.com/misapp/GetReports.do?reportTypeId=12300&reportTitle=LMPs%20by%20Resource%20Nodes,%20Load%20Zones%20and%20Trading%20Hubs&showHTMLView=&mimicKey"
-#SAVE_PATH = "/Users/veronicayarovinsky/Desktop/ERCOT-data"
-SAVE_PATH = "/Users/veronicayarovinsky/Desktop/ERCOT-web-scraper/ERCOT-data"
+# SAVE_PATH = "/Users/veronicayarovinsky/Desktop/ERCOT-web-scraper/ERCOT-data"
+SAVE_PATH = os.getcwd() + "/ERCOT-data"
 
 def main():
     new_downloads = scrape()            # scrape webpage for urls to download
     download_files(new_downloads)       # downloads files in reverse order (oldest to most recent)
-    print("Downloaded " + str(len(new_downloads)) + " new files.")
+    print("Downloaded " + str(len(new_downloads)) + " new files to " + SAVE_PATH + ".")
 
 
 def scrape():
@@ -29,9 +28,10 @@ def scrape():
 
     new_data = True
 
+    print("Locating files to donwload ...")
+
     if len(os.listdir(SAVE_PATH) ) != 0:        # if files have previously been downloaded
         last_download = get_last_download()     # prevents duplicate downloads
-        print(last_download)
 
         while new_data == True:
             for table in page.find_all('tr'):                           # prevents duplicates
@@ -46,8 +46,8 @@ def scrape():
                         else:
                             new_downloads.append(download_url)
 
-    else:
-        for table in page.find_all('tr'):                           # prevents duplicates
+    else:                                       # folder is empty
+        for table in page.find_all('tr'):                               # prevents duplicates
             for row in table.find_all('tr'):
                 if 'xml' not in str(row) and 'href' in str(row) and new_data == True:
                     download_link = row.find('a').get('href')
@@ -59,6 +59,7 @@ def scrape():
 
 
 def download_files(new_downloads):
+    print("Downloading the files ...")
     for url in reversed(new_downloads):
         zip = zipfile.ZipFile(io.BytesIO(url.content))
         zip.extractall(SAVE_PATH)
